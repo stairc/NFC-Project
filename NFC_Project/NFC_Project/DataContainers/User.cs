bbc98fa8bb3101f45df3bd2e1eec7f4d34a74df1
Cameron Stair <cameronstair@yahoo.com>
2017-03-15 11:04:37 -0400

    Added User Data container to hold data in the User Table

---
+++ NFC_Project/NFC_Project/DataContainers/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFC_Project.DataContainers
+{
+    class User
+    {
+        public string UniqueID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public User(string id, string first, string last, string email, string phone)
+        {
+            UniqueID = id;
+            FirstName = first;
+            LastName = last;
+            EmailAddress = email;
+            PhoneNumber = phone;
+        }
+    }
+}
