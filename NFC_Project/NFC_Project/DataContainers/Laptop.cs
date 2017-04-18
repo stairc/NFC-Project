c3ef80f5493534de0013ca07ffeb41aae40b765c
Cameron Stair <staircd@miamioh.edu>
2017-04-18 14:12:14 -0400

    Added comments based on owner feedback

--- NFC_Project/NFC_Project/DataContainers/Laptop.cs
+++ NFC_Project/NFC_Project/DataContainers/Laptop.cs
@@ -10,17 +10,17 @@ namespace NFC_Project.DataContainers
     {
         public string LaptopID { get; set; }
         public string SerialNumber { get; set; }
-        public string Condition { get; set; }
+        public string Condition { get; set; } // Change to type of device // Add to table
         public string Brand { get; set; }
         public string Model { get; set; }
-        public string Processor { get; set; }
-        public string RAM { get; set; }
+        public string Processor { get; set; } //
+        public string RAM { get; set; } //
         public string Resolution { get; set; }
         public string ScreenSize { get; set; }
         public DateTime DateAdded { get; set; }
         public bool InService { get; set; }
         public string Memory { get; set; }
-        public string OSVersion { get; set; }
+        public string OSVersion { get; set; } //
 
         public Laptop(string id, string serial, string cond, string brand,
                       string model, string processor, string ram, string resolution,
